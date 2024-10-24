3a0add8f282f76438c1112ab7e335800aca66871
JavierMoreno <jmorenom46@educarex.es>
2024-10-24 19:52:39 +0200

    commit TareaParejas

--- TareaParejas/script.js
+++ TareaParejas/script.js
@@ -1,14 +1,12 @@
-let cartas = ["A", "B", "C", "D", "B", "A", "D", "C"];
+let cartas = ["🍎", "🍉", "🍋", "🍎", "🍋", "🍉", "🫐", "🫐"];
 let cartasMostradas = [];
 let mostradas = 0;
 let container = document.getElementById("container");
-console.log(container);
-
-cartas.forEach((letra) => {
+let aciertos = 0;
+cartas.forEach((fruta) => {
   let carta = document.createElement("div");
   carta.classList.add("carta", "escondida");
-  carta.dataset.letra = letra;
-  console.log(carta);
+  carta.dataset.fruta = fruta;
   carta.onclick = function () {
     mostrarCarta(carta);
   };
@@ -16,21 +14,30 @@ cartas.forEach((letra) => {
 });
 
 function mostrarCarta(carta) {
-  if (mostradas < 2 && tarjeta.classList.contains("escondida")) {
+  if (mostradas < 2 && carta.classList.contains("escondida")) {
     carta.classList.remove("escondida");
     carta.classList.add("mostrada");
     carta.innerText = carta.dataset.fruta;
-    cartasMostradas.push(carta);
+    console.log("push carta");
+    cartasMostradas[mostradas] = carta;
     mostradas++;
     if (mostradas === 2) {
       setTimeout(verificarPareja, 1000);
+      mostradas = 0;
     }
   }
 }
 
 function verificarPareja() {
-  if (cartasMostradas[0] === cartasMostradas[1]) {
-    cartasMostradas = [];
+  if (cartasMostradas[0].dataset.fruta === cartasMostradas[1].dataset.fruta) {
+    for (let i = 0; i < 2; i++) {
+      cartasMostradas.splice(0, 1);
+      console.log(cartasMostradas);
+    }
+    aciertos += 2;
+    if (aciertos == cartas.length) {
+      alert("Ganaste");
+    }
   } else {
     cartasMostradas.forEach((carta) => {
       carta.classList.remove("mostrada");
